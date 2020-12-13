839ec7268461aaea06da243282a5a234099d99e6
daienelima <daiene.m17@gmail.com>
2020-12-13 16:08:01 -0300

    ajuste

--- src/main/java/com/daiene/consumo/interfaces/CepService.java
+++ src/main/java/com/daiene/consumo/interfaces/CepService.java
@@ -1,14 +1,14 @@
 package com.daiene.consumo.interfaces;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.daiene.consumo.model.Endereco;
 
 @FeignClient(url= "https://viacep.com.br/ws/" , name = "viacep")
 public interface CepService {
 	
-	@RequestMapping("{cep}/json")
+	@GetMapping("{cep}/json")
     Endereco buscaEnderecoPorCep(@PathVariable("cep") String cep);
 }
